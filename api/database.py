9616116fb8f9906097fdd3defbd72d8323558e0c
kua-306 <thu3062003@gmail.com>
2026-03-27 11:00:40 +0700

    change db

--- api/database.py
+++ api/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'postgresql://postgres:ntltcua3006@localhost/history'
+URL_DATABASE = "sqlite+aiosqlite:///./sql_app.db"
 
-engine = create_engine(URL_DATABASE)
+engine = create_engine(URL_DATABASE,connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit = False,autoflush = False, bind = engine)
 Base = declarative_base()
 def get_db():
