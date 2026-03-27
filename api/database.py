ce6dc6e4aa8ea1adf49175295067c2b4a175627e
kua-306 <thu3062003@gmail.com>
2026-03-27 11:32:52 +0700

    fix engine

--- api/database.py
+++ api/database.py
@@ -1,11 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_async_engine,AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 URL_DATABASE = "sqlite+aiosqlite:///./sql_app.db"
 
-engine = create_engine(URL_DATABASE,connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit = False,autoflush = False, bind = engine)
+engine = create_async_engine(URL_DATABASE,connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit = False,autoflush = False,class_=AsyncSession, bind = engine)
 Base = declarative_base()
 def get_db():
     db = SessionLocal()
